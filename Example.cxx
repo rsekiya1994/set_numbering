6444be84d596ed7a74c103f31102d3c23b0ddeda
rsekiya1994 <71419702+rsekiya1994@users.noreply.github.com>
2020-11-10 01:39:47 +0000

    Update Example.cxx

--- Example.cxx
+++ Example.cxx
@@ -4,14 +4,15 @@
 
 int main(){
   sky::set_numbering<int, std::string> A;
-  A.Add(1, "a");
-  A.Add(2, "a");
-  A.Add(3, "b");
-  std::cout << "(1, \"a\") : " << A.Index(1, "a") << std::endl;
-  std::cout << "(2, \"a\") : " << A.Index(2, "a") << std::endl;
-  std::cout << "(1, \"b\") : " << A.Index(1, "b") << std::endl;
+  A.add(1, "a"); // 要素を加えていく。
+  A.add(2, "a");
+  A.add( {3, "b"} ); //初期化子リストでも可
+  std::cout << "(1, \"a\") : " << A.index(1, "a") << std::endl; // {1, "a"}のindexを取得
+  std::cout << "(2, \"a\") : " << A.index(2, "a") << std::endl; // {2, "a"}のindexを取得
+  std::cout << "(1, \"b\") : " << A.index( {1, "b"} ) << std::endl; // 初期化子リストでも検索可。{1, "b"}はaddされてないので -1 が返る。
   for(int i = 0 ; i < 3 ; i++){
-    std::cout << A.GetArg<0>(i)	<< std::endl;
+    // indexから集合の要素を取得
+    std::cout << A.get_arg<0>(i) << " " << A.get_arg<1>(i) << std::endl;
   }
   return 0;
 }
@@ -20,6 +21,6 @@ int main(){
 // (1, "a") : 0
 // (2, "a") : 1
 // (1, "b") : -1
-// 1
-// 2
-// 3
+// 1 a
+// 2 a
+// 3 b
